9d891692ef7aec444496a8dadb1e8f7cd577fc33
Vitalij Mik <BlackScorp@users.noreply.github.com>
2026-03-20 10:26:01 +0100

    NTR: Fix POS Terminal Webhooks (#1231)
    * NTR: fixed Webhooks for POS Terminals
    
    * NTR: fix POS Webhooks
    
    * NTR: add migration to fix old orders
    
    ---------
    
    Co-authored-by: vitalijmik <vitalij.mik@dasistweb.de>

--- src/Controller/Storefront/Webhook/NotificationFacade.php
+++ src/Controller/Storefront/Webhook/NotificationFacade.php
@@ -181,13 +181,14 @@ class NotificationFacade
         $molliePayment = null;
         $mollieOrder = null;
 
-        if (! empty($orderAttributes->getMollieOrderId())) {
+        if (! empty($mollieOrderId)) {
             // fetch the order of our mollie ID
             // from our sales channel mollie profile
+
             $mollieOrder = $this->gatewayMollie->getOrder($mollieOrderId);
             $molliePayment = $this->statusConverter->getLatestPayment($mollieOrder);
             $status = $this->statusConverter->getMollieOrderStatus($mollieOrder);
-        } elseif ($orderAttributes->isTypeSubscription()) {
+        } elseif ($orderAttributes->isTypeSubscription() || ! empty($molliePaymentId)) {
             // subscriptions are automatically charged using a payment ID
             // so we do not have an order, but a payment instead
             $molliePayment = $this->gatewayMollie->getPayment($molliePaymentId);
